b6de2586b2aaa498ff85f2ab6859473de7aab2bb
truong2501 <nguyentruong250101@gmail.com>
2023-09-22 16:04:09 +0700

    edit

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -37,11 +37,20 @@ module.exports.index = async (req, res) => {
 	)
 
 	// console.log(objectPagination.currentPage);
-	// End Pagination
+	// End 
+	
+	// Sort
+	let sort = {};
+	if(req.query.sortKey && req.query.sortValue) {
+		sort[req.query.sortKey] = req.query.sortValue;
+	} else {
+		sort.position = "desc";
+	}
+	//End Sort
 
 
 	const products = await Product.find(find)
-		.sort({ position: "desc" })
+		.sort(sort)
 		.limit(objectPagination.limitItems)
 		.skip(objectPagination.skip);
 
